3c8fdd316743d51a2093425021eb8f7708a2dbba
[dyad] <jadalarab15@hotmail.com>
2025-12-01 17:08:36 +0200

    [dyad] Updated CORS to allow any origin - wrote 1 file(s)

--- Incident/Program.cs
+++ Incident/Program.cs
@@ -84,14 +84,15 @@ builder.Services.AddScoped<ILookupService, LookupService>();
 builder.Services.AddScoped<IIncidentStatusService, IncidentStatusService>();
 builder.Services.AddScoped<DbSeederService>();
 
-// Add CORS
+// Add CORS - Allow all origins, methods, and headers
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
         policy.AllowAnyOrigin()
               .AllowAnyMethod()
-              .AllowAnyHeader();
+              .AllowAnyHeader()
+              .WithExposedHeaders("*");
     });
 });
 
@@ -112,7 +113,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// CORS must be called before Authentication and Authorization
 app.UseCors("AllowAll");
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
